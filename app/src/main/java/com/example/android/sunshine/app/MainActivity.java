2510d5f1055af76528053cba820f44f21f8b0f8e
tenprint <mrlip@hotmail.com>
2015-10-04 23:56:15 -0400

    In progress - Lesson 5 Build 2-pane Tablet UI

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -16,10 +16,12 @@
 package com.example.android.sunshine.app;
 
 import android.content.Intent;
+import android.graphics.Point;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -27,19 +29,47 @@ public class MainActivity extends ActionBarActivity {
 
     private final String LOG_TAG = MainActivity.class.getSimpleName();
     private final String FORECASTFRAGMENT_TAG = "FFTAG";
+    private static final String DETAILFRAGMENT_TAG = "DFTAG";
 
     private String mLocation;
 
+    boolean mTwoPane;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        mLocation = Utility.getPreferredLocation(this);
         super.onCreate(savedInstanceState);
+        mLocation = Utility.getPreferredLocation(this);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new ForecastFragment(), FORECASTFRAGMENT_TAG)
-                    .commit();
-        }
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        Log.d(LOG_TAG, "display size = " + size.toString());
+        //Toast.makeText(this, size.toString(), Toast.LENGTH_SHORT).show();
+
+//
+//        if (findViewById(R.id.weather_detail_container) != null) {
+//            // The detail container view will be present only in the large-screen layouts
+//            // (res/layout-sw600dp). If this view is present, then the activity should be
+//            // in two-pane mode.
+//            mTwoPane = true;
+//            // In two-pane mode, show the detail view in this activity by
+//            // adding or replacing the detail fragment using a
+//            // fragment transaction.
+//            if (savedInstanceState == null) {
+//                getSupportFragmentManager().beginTransaction()
+//                        .replace(R.id.weather_detail_container, new DetailFragment(), DETAILFRAGMENT_TAG)
+//                        .commit();
+//            }
+//        } else {
+//            mTwoPane = false;
+//        }
+
+        mTwoPane = true;
+
+        Log.d(LOG_TAG,"mTwoPane = "+mTwoPane);
+
+
     }
 
     @Override
@@ -95,7 +125,7 @@ public class MainActivity extends ActionBarActivity {
         String location = Utility.getPreferredLocation( this );
         // update the location in our second pane using the fragment manager
         if (location != null && !location.equals(mLocation)) {
-            ForecastFragment ff = (ForecastFragment)getSupportFragmentManager().findFragmentByTag(FORECASTFRAGMENT_TAG);
+            ForecastFragment ff = (ForecastFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_forecast);
             if ( null != ff ) {
                 ff.onLocationChanged();
             }
