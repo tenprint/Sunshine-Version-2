64f8d83371a198e0d1f13babb022ed94b71bbd71
tenprint <mrlip@hotmail.com>
2015-11-03 09:05:41 -0500

    Lesson 5: Custom Views - in progress -- added hard coded values to onMeasure()

--- app/src/main/java/com/example/android/sunshine/app/MyView.java
+++ app/src/main/java/com/example/android/sunshine/app/MyView.java
@@ -1,4 +1,4 @@
-package com.example.android.sunshine.app.data;
+package com.example.android.sunshine.app;
 
 import android.content.Context;
 import android.util.AttributeSet;
@@ -8,6 +8,11 @@ import android.view.View;
  * Created by jlipata on 10/15/15.
  */
 public class MyView extends View {
+
+    //Width and height variables
+    int mWidth = 200;
+    int mHeight = 200;
+
     public MyView(Context context){
         super(context);
     }
@@ -24,5 +29,6 @@ public class MyView extends View {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(mWidth, mHeight);
     }
 }
